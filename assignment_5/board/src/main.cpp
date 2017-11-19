67139969ccb28d3022cd0f39ac67705e1f8d32ba
J.B. Lanier <johnblanier@gmail.com>
2017-11-19 15:57:21 -0800

    start hw5

---
+++ assignment_5/board/src/main.cpp
@@ -0,0 +1,351 @@
+#include "Arduino.h"
+#include "secrets.h"
+#include "helpers.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include "MAX30105.h"
+#include "BigNumber.h"
+
+#include "SparkFunLIS3DH.h"
+#include "SPI.h"
+LIS3DH myIMU(SPI_MODE, 0);
+
+const int SAMPLE_RATE = 50;
+const int NUM_SAMPLES_PER_POST = 50; // Not Necessarily tied to Sample Rate
+const int MAX_SAMPLE_TRIPLET_SIZE = 81; // max size example: {"t":1508735981516,"ir":262143,"r":262143,"x":-23.0026,"y":-23.0026,"z":-23.0026}
+const int MAX_SAMPLES_ARRAY_SIZE = ( (MAX_SAMPLE_TRIPLET_SIZE + 1) * NUM_SAMPLES_PER_POST ) + 1;
+const int MAX_JSON_STR_SIZE = MAX_SAMPLES_ARRAY_SIZE + 30;
+
+char json_str[MAX_JSON_STR_SIZE];
+size_t json_offset = 0;
+
+MAX30105 particleSensor;
+byte powerLevel = 0x1F;
+//Options: 0=Off to 255=50mA 
+//powerLevel = 0x02, 0.4mA - Presence detection of ~4 inch
+//powerLevel = 0x1F, 6.4mA - Presence detection of ~8 inch
+//powerLevel = 0x7F, 25.4mA - Presence detection of ~8 inch
+//powerLevel = 0xFF, 50.0mA - Presence detection of ~12 inch
+
+String deviceName = "CS244";
+const int HTTP_PORT = 80;
+WiFiClient client;
+bool aWriteHasFailed = false;
+
+
+long hz_startTime; //Used to calculate Hz, reset often
+int hz_samples_taken = 0; //Used to calculate Hz, reset often
+
+int samples_taken = 0; //Reset for each POST to server
+
+long unblockedValue; //Average IR at power up
+
+void resetJsonString() {
+  json_offset = 0;
+  char *pwr;
+  switch (powerLevel) {
+    case 0x02: pwr = "0.4"; break;
+    case 0x1F: pwr = "6.4"; break;
+    case 0x7F: pwr = "25.4"; break;
+    case 0xFF: pwr = "50.0"; break;
+    default: pwr = "unkown"; break;
+  }
+  json_offset += sprintf(json_str, "{\"pwr\":\"%s\",\"samples\":[",pwr);
+}
+
+// function to display a big number and free it afterwards
+void printBignum (BigNumber & n)
+{
+  char *s = n.toString();
+  Serial.println(s);
+  free(s);
+}  // end of printBignum
+
+// Returns only when connected to Wifi
+void connectToWifi() {
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+
+        for (int i = 0; i < 3; i++) {
+          digitalWrite(LED_BUILTIN, LOW);
+          delay(400);
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(400);
+        }
+    } 
+}
+
+// Returns only when connected to the Server, connects to Wifi if Necessary
+void connectToServer() {
+  while (!client.connected()) {
+    if (WiFi.status() != WL_CONNECTED) {
+      connectToWifi();
+    }
+    client.connect(SERVER_HOSTNAME, 80);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+  }
+  client.setNoDelay(1);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// Speedy, relies on using existing connection to server, can send multiple packets.
+// Doesn't even look at response, doesn't care.
+void doBetterPost(const char *path, char *json_content, size_t json_size) {
+  String endpointURL = String("http://") + SERVER_HOSTNAME + path;
+  
+  // create the request and headers
+  String request = "POST " + endpointURL + " HTTP/1.1\r\n" +
+    "Host: " + String(SERVER_HOSTNAME) + "\r\n" + 
+    "Accept: application/json" + "\r\n" + 
+    "Connection: Keep-Alive\r\n" +
+    "Content-Type: application/json\r\n" +
+    "Content-Length: " + String(json_size) + "\r\n\r\n";
+  client.flush();
+  client.print(request);
+
+  size_t c_offset = 0;
+  while (c_offset < json_size) {
+    client.flush();
+    const uint8_t *json_content_at_offset = (const uint8_t *) &json_content[c_offset];
+    size_t result = client.write(json_content_at_offset, WIFICLIENT_MAX_PACKET_SIZE < (json_size - c_offset) ? WIFICLIENT_MAX_PACKET_SIZE : (json_size - c_offset) );
+    if (result > 0) {
+      c_offset += result;
+    } else {
+      Serial.println("write failed");
+      aWriteHasFailed = true;
+      return;
+    }
+  }
+  //Serial.println(aWriteHasFailed);
+}
+
+BigNumber getCurrentTime() {
+  static BigNumber timestamp_start = BigNumber(-1); //timestamp taken from server at startup
+  static long system_millis_at_server_calibration;
+
+  if (timestamp_start == -1) {
+    if (WiFi.status() != WL_CONNECTED) {
+      connectToWifi();
+    }
+  
+    String url = String("http://") + SERVER_HOSTNAME + "/time";
+    Serial.println(String("POST to ") + url);
+    if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+      
+        HTTPClient http;    //Declare object of class HTTPClient
+        http.begin(url);    //Specify request destination
+        long request_start = millis();
+        int httpCode = http.GET(); //Send the request
+        String payload = http.getString();                  //Get the response payload
+        Serial.println("response code: " + String(httpCode));   //Print HTTP return code
+        Serial.println("response data: " + payload + "\n\n");    //Print request response payload
+        http.end();  //Close connection
+        
+        if (httpCode != 200) {
+          Serial.print("HTTP Code for getting time: ");
+          Serial.println(httpCode);
+          return BigNumber(-1);
+        }
+  
+        system_millis_at_server_calibration = millis();
+         // factor in latency for timestamp to be sent back over internet to the board.
+        timestamp_start = BigNumber(payload.c_str()) + BigNumber((system_millis_at_server_calibration - request_start)/2);
+
+      } else {
+         Serial.println("WiFi not connected, POST aborted.");   
+         return BigNumber(-1);
+      }
+  }
+
+  return BigNumber(timestamp_start + BigNumber(millis() - system_millis_at_server_calibration));
+}
+
+void setup() {
+
+//   // We use bignumber to handle full-size timestamps
+  BigNumber::begin(0); //set constructor with max number of decimal digits we care about
+
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Baud rate
+  Serial.begin(115200);
+  Serial.println("Program started");
+ 
+  // We'll use this connection later to doBetterPost() with the header, "connection: Keep-Alive"
+  connectToWifi();
+
+  // Print some fun internet stats
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+  byte mac[6];
+  WiFi.macAddress(mac);
+  char MAC_char[18]="";
+  for (int i = 0; i < sizeof(mac); ++i) {
+      sprintf(MAC_char, "%s%02x:", MAC_char, mac[i]);
+  }
+  Serial.print("Mac address: ");
+  Serial.print(MAC_char);
+  Serial.println("\n");
+
+
+  myIMU.fifoBegin();
+  uint8_t returnData = myIMU.begin();
+  if (( returnData != 0x00) && (returnData != 0xFF)) {
+    while(1) {
+      Serial.println("Problem starting Accelerometer.");
+      delay(1000);
+    }
+  }
+
+
+   // Say hi to the MAX30105 Particle Sensor
+   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+   {
+     while(1) {
+       Serial.println("MAX30105 was not found. Please check wiring/power. ");
+       delay(1000);
+     }
+   }
+
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 50; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 2048; //Options: 2048, 4096, 8192, 16384
+  particleSensor.setup(powerLevel, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  //Take an average of IR readings at power up to calibrate
+  unblockedValue = 0;
+  for (byte x = 0 ; x < 32 ; x++)
+  {
+    unblockedValue += particleSensor.getIR(); //Read the IR value
+  }
+  unblockedValue /= 32;
+
+  resetJsonString();
+  
+  while (getCurrentTime() == -1) {
+    Serial.println("Failed to calibrate time with server.");
+    delay(5000);
+  }
+  connectToServer();
+
+  // Do time sensitive operations last
+  hz_startTime = millis();
+
+
+
+  particleSensor.check(); // read in the first (up to 3) samples
+
+}
+
+void loop() {
+
+  //Get all parameters
+
+  // if(!client.connected()) {
+  //   Serial.println("Waiting to connect to server");
+  //   connectToServer();
+  // }
+
+  long frame_start = millis();
+  samples_taken++;
+  hz_samples_taken++;
+
+  float x = myIMU.readFloatAccelX();
+  float y = myIMU.readFloatAccelY();
+  float z = myIMU.readFloatAccelZ();
+  Serial.print("X = ");
+  Serial.print(x, 4);
+  Serial.print(" Y = ");
+  Serial.print(y, 4);
+  Serial.print(" Z = ");
+  Serial.print(z, 4);
+
+  char *ir = BigNumber(particleSensor.getFIFOIR()).toString();
+  char *r = BigNumber(particleSensor.getFIFORed()).toString();
+  
+  char *time_str = getCurrentTime().toString();
+  
+  char *json_str_at_offset = &json_str[json_offset];
+  
+  json_offset += sprintf(json_str_at_offset, "{\"t\":%s,", time_str);
+  json_str_at_offset = &json_str[json_offset];  
+  json_offset += sprintf(json_str_at_offset, "\"ir\":%s,", ir);
+  json_str_at_offset = &json_str[json_offset];  
+  json_offset += sprintf(json_str_at_offset, "\"r\":%s,", r);
+  json_str_at_offset = &json_str[json_offset];
+
+  int accel_str_len = 7;
+  char accel_str[accel_str_len];
+  dtostrf(x, accel_str_len, 4, accel_str);  
+  json_offset += sprintf(json_str_at_offset, "\"x\":%s,", accel_str);
+  json_str_at_offset = &json_str[json_offset];  
+  dtostrf(y, accel_str_len, 4, accel_str);  
+  json_offset += sprintf(json_str_at_offset, "\"y\":%s,", accel_str);
+  json_str_at_offset = &json_str[json_offset];  
+  dtostrf(z, accel_str_len, 4, accel_str);  
+  json_offset += sprintf(json_str_at_offset, "\"z\":%s},", accel_str);
+  
+
+  //Real time readings
+  Serial.print(" R[");
+  Serial.print(r);
+  Serial.print("] IR[");
+  Serial.print(ir);
+  Serial.print("] Hz[");
+  Serial.print((float)hz_samples_taken / ((millis() - hz_startTime) / 1000.0), 2);
+  Serial.print("]");
+
+  Serial.println();
+
+  free(time_str);
+  free(ir);
+  free(r);
+  
+
+  particleSensor.nextSample(); //We're finished with this sample so move to next sample
+  if (!particleSensor.available()) { //are we out of new data?
+    particleSensor.check(); //Check the sensor, read up to 3 samples
+  }
+
+  //reset these stats to keep fps counter fresh
+  if (hz_samples_taken >= 180) {
+    hz_samples_taken = 0;
+    hz_startTime = millis();
+  }
+
+  if (samples_taken >= NUM_SAMPLES_PER_POST) {
+
+    json_str[json_offset-1] = ']';
+    json_str_at_offset = &json_str[json_offset];
+    json_offset += sprintf(json_str_at_offset, "}\0");
+    
+    //Serial.write(json_str,json_offset);
+    //Serial.println("");
+    doBetterPost("/ppg_accel_samples",json_str, json_offset);
+    resetJsonString();
+    samples_taken = 0;
+  }
+
+  //Ensure a max rate of 50Hz
+  long frame_duration = millis() - frame_start;
+  long wait_time = 20 - frame_duration;
+  if (wait_time > 0) {
+    delay(wait_time);
+  }
+  
+  // // //Print frame duration if we POSTed
+  // // if (samples_taken == 0) {
+  // //   Serial.println(frame_duration);    
+  // // }
+  
+}
+
---
+++ assignment_5/server/app.js
@@ -0,0 +1,231 @@
+//require the express nodejs module
+var express = require('express');
+//set an instance of exress
+var app = express();
+//require the body-parser nodejs module
+var bodyParser = require('body-parser');
+//require the path nodejs module
+var path = require("path");
+var json2csv = require('json2csv');
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var mongodb = null;
+
+//support parsing of application/json type post data
+app.use(bodyParser.json());
+ 
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
+
+// getDateTime from stackoverflow: https://stackoverflow.com/questions/7357734/how-do-i-get-the-time-of-day-in-javascript-node-js
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
+app.post('/', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+ 
+    mongodb.collection("messages").insertOne(req.body);
+
+    res.send(JSON.stringify({
+        Greeting: 'Yo Dude'
+    }));
+ 
+    //debugging output for the terminal
+    console.log('Received post: ' + JSON.stringify(req.body));
+});
+
+app.post('/samples', function(req, res){
+    var collection = mongodb.collection("samples");
+    collection.findOne({"pwr":req.body.pwr}, function(err, result) {
+        if (err) throw err;
+        if (result) {
+            console.log("Received " + req.body.samples.length +" samples for " + req.body.pwr + " " + getDateTime());
+            collection.update(
+                { "_id": result._id},
+                {
+                    "$push": {
+                        "samples": {
+                            "$each": req.body.samples
+                        }
+                    }
+                }
+            )
+        } else {
+            console.log("Received " + req.body.samples.length +" samples for " + req.body.pwr + " (NEW) " + getDateTime());
+            collection.insertOne(req.body);
+        }
+    });
+    res.sendStatus(200);
+});
+
+app.post('/ppg_accel_samples', function(req, res){
+    var collection = mongodb.collection("samples2");
+    collection.findOne({"pwr":req.body.pwr}, function(err, result) {
+        if (err) throw err;
+        if (result) {
+            console.log("Received " + req.body.samples.length +" samples for " + req.body.pwr + " " + getDateTime());
+            collection.update(
+                { "_id": result._id},
+                {
+                    "$push": {
+                        "samples": {
+                            "$each": req.body.samples
+                        }
+                    }
+                }
+            )
+        } else {
+            console.log("Received " + req.body.samples.length +" samples for " + req.body.pwr + " (NEW) " + getDateTime());
+            collection.insertOne(req.body);
+        }
+    });
+    res.sendStatus(200);
+});
+
+app.get('/hw2csv', function(req, res){
+    var collection = mongodb.collection("samples");
+    var number_of_samples = 6000;
+    //{pwr:"6.4"}, { samples: { $slice: -5 } }
+    collection.findOne({pwr:"0.4"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result1) {
+        collection.findOne({pwr:"6.4"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result2) {
+            collection.findOne({pwr:"25.4"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result3) {
+                collection.findOne({pwr:"50.0"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result4) {
+                    var combined_columns = [];
+                    // console.log(number_of_samples);
+                    // console.log(result1.samples.length);
+                    for(var i=0; i<result1.samples.length; i++){
+                        combined_columns.push({
+                            IR1:result1.samples[i].ir,
+                            RED1:result1.samples[i].r,
+                            IR2:result2.samples[i].ir,
+                            RED2:result2.samples[i].r,
+                            IR3:result3.samples[i].ir,
+                            RED3:result3.samples[i].r,
+                            IR4:result4.samples[i].ir,
+                            RED4:result4.samples[i].r
+                        });
+                    }
+
+                    // console.log(combined_columns);
+                    var fields = ['IR1','RED1','IR2','RED2','IR3','RED3','IR4','RED4',];
+                    var csv = json2csv({ data: combined_columns, fields: fields });
+                    res.setHeader('Content-disposition', 'attachment; filename=team8_assignment2.csv');
+                    res.set('Content-Type', 'text/csv');
+                    res.status(200).send(csv);
+                });
+            });
+        });
+    });
+
+});
+
+app.get('/hw4csv', function(req, res){
+    var collection = mongodb.collection("samples2");
+    var number_of_samples = 250;
+    collection.findOne({pwr:"6.4"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result) {
+
+        var columns = [];
+        for(var i=0; i<result.samples.length; i++){
+            columns.push({
+                X:result.samples[i].x,
+                Y:result.samples[i].y,
+                Z:result.samples[i].z
+            });
+        }
+
+        // console.log(combined_columns);
+        var fields = ['X','Y','Z'];
+        var csv = json2csv({ data: columns, fields: fields });
+        res.setHeader('Content-disposition', 'attachment; filename=team8_assignment4.csv');
+        res.set('Content-Type', 'text/csv');
+        res.status(200).send(csv);
+
+    });
+
+});
+
+app.get('/time', function(req, res){
+    res.send((new Date).getTime().toString());
+});
+
+app.get('/hw6raw_csv', function(req, res){
+    var collection = mongodb.collection("samples2");
+    var number_of_samples = 250; //50*60*10
+    collection.findOne({pwr:"6.4"}, { samples: { $slice: (number_of_samples*-1) } }, function(err, result) {
+
+        var columns = [];
+        for(var i=0; i<result.samples.length; i++){
+            columns.push({
+                IR:result.samples[i].ir,
+                RED:result.samples[i].r,
+                X:result.samples[i].x,
+                Y:result.samples[i].y,
+                Z:result.samples[i].z
+            });
+        }
+
+        // console.log(combined_columns);
+        var fields = ['X','Y','Z'];
+        var csv = json2csv({ data: columns, fields: fields });
+        res.setHeader('Content-disposition', 'attachment; filename=team8_assignment4.csv');
+        res.set('Content-Type', 'text/csv');
+        res.status(200).send(csv);
+
+    });
+
+});
+
+var port = 80;
+// Connection URL
+var url = 'mongodb://localhost:27017/hw2';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  if (err != null) {
+  	console.log("Failed to connect to Mongo: " + err);
+  	assert(false);
+  }
+
+  console.log("Connected successfully to Mongo at " + url);
+  mongodb = db;
+
+  //wait for a connection
+  var server = app.listen(port, function () {
+  	console.log('HTTP Server is running. Point your browser to: http://localhost:' + port);
+  });
+
+    server.on('connection', function(socket) {
+        console.log("A new connection was made by a client.");
+        socket.setTimeout(30 * 1000);
+        // 30 second timeout. Change this as you see fit.
+    })
+});
+
+
+
+
